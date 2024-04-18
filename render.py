07be38b1363bf9d3c61ada9eebbd5ed118caf88b
Yingwei Song <56505931+wrencanfly@users.noreply.github.com>
2024-04-18 07:13:31 -0400

    Update render.py
    support for changing encoder dim

--- render.py
+++ render.py
@@ -32,6 +32,9 @@ def render_set(model_path, source_path, name, iteration, views, gaussians, pipel
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
 
+    import numpy as np
+    from sklearn.decomposition import PCA
+    
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
         output = render(view, gaussians, pipeline, background, args)
 
@@ -39,25 +42,59 @@ def render_set(model_path, source_path, name, iteration, views, gaussians, pipel
             rendering = output["render"]
         else:
             rendering = output["language_feature_image"]
+            # avg
+            # first_two = rendering[:2]
+            # remaining_average = torch.mean(rendering[2:], dim=0, keepdim=True)
+            # rendering = torch.cat([first_two, remaining_average], dim=0)
+            #rendering = torch.stack([rendering[0], rendering[1], (rendering[2] + rendering[3])/2], dim=0)
+            
+
+
+
             
         if not args.include_feature:
             gt = view.original_image[0:3, :, :]
             
         else:
             gt, mask = view.get_language_feature(os.path.join(source_path, args.language_features_name), feature_level=args.feature_level)
+            
+            #avg
+            # first_two = gt[:2]
+            # remaining_average = torch.mean(gt[2:], dim=0, keepdim=True)
+            # gt = torch.cat([first_two, remaining_average], dim=0)
+            
+
+            
 
         np.save(os.path.join(render_npy_path, '{0:05d}'.format(idx) + ".npy"),rendering.permute(1,2,0).cpu().numpy())
         np.save(os.path.join(gts_npy_path, '{0:05d}'.format(idx) + ".npy"),gt.permute(1,2,0).cpu().numpy())
+        
+        # for generate rgb image, we need to PCA to 3 dim
+        
+        if args.include_feature:
+            
+            rendering_reshaped = rendering.view(rendering.shape[0], -1).cpu().numpy()
+            pca = PCA(n_components=3)
+            rendering_pca = pca.fit_transform(rendering_reshaped.T) 
+            rendering_pca_image = rendering_pca.T.reshape(3, rendering.shape[1], rendering.shape[2])
+            rendering = torch.from_numpy(rendering_pca_image).to("cuda")  
+            
+            gt_reshaped = gt.view(gt.shape[0], -1).cpu().numpy()
+            pca = PCA(n_components=3)
+            gt_pca = pca.fit_transform(gt_reshaped.T)
+            gt_pca_image = gt_pca.T.reshape(3, gt.shape[1], gt.shape[2]) 
+            gt = torch.from_numpy(gt_pca_image).to("cuda")  
+            
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
                
-def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, args):
+def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, args, language_feature_dim : int):
     with torch.no_grad():
         gaussians = GaussianModel(dataset.sh_degree)
         scene = Scene(dataset, gaussians, shuffle=False)
         checkpoint = os.path.join(args.model_path, 'chkpnt30000.pth')
         (model_params, first_iter) = torch.load(checkpoint)
-        gaussians.restore(model_params, args, mode='test')
+        gaussians.restore(model_params, args, mode='test', language_feature_dim=language_feature_dim)
         
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
@@ -79,10 +116,10 @@ if __name__ == "__main__":
     parser.add_argument("--skip_test", action="store_true")
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--include_feature", action="store_true")
-
+    parser.add_argument("--language_feature_dim", type=int, default=3)
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
 
     safe_state(args.quiet)
 
-    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args)
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args, args.language_feature_dim)
