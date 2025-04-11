31898a918d1a05c27dab3f713e5fec797d5b42ac
Christian Rauch <Christian.Rauch@unileoben.ac.at>
2025-04-11 15:27:58 +0200

    fix training script

--- process.sh
+++ process.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # get the language feature of the scene
-python preprocess.py --dataset_name $dataset_path
+python preprocess.py --dataset_path $dataset_path
 
 # train the autoencoder
 cd autoencoder
-python train.py --dataset_path $dataset_path --encoder_dims 256 128 64 32 3 --decoder_dims 16 32 64 128 256 256 512 --lr 0.0007 --dataset_name ae_ckpt
+python train.py --dataset_path $dataset_path --encoder_dims 256 128 64 32 3 --decoder_dims 16 32 64 128 256 256 512 --lr 0.0007 --dataset_name $dataset_name
 # e.g. python train.py --dataset_path ../data/sofa --encoder_dims 256 128 64 32 3 --decoder_dims 16 32 64 128 256 256 512 --lr 0.0007 --dataset_name sofa
 
 # get the 3-dims language feature of the scene
-python test.py --dataset_name $dataset_path --dataset_name $dataset_name
+python test.py --dataset_path $dataset_path --dataset_name $dataset_name
 # e.g. python test.py --dataset_path ../data/sofa --dataset_name sofa
+
+cd ..
 
 # ATTENTION: Before you train the LangSplat, please follow https://github.com/graphdeco-inria/gaussian-splatting
 # to train the RGB 3D Gaussian Splatting model.
@@ -18,7 +20,7 @@ python test.py --dataset_name $dataset_path --dataset_name $dataset_name
 
 for level in 1 2 3
 do
-    python train.py -s $dataset_path -m output/${casename} --start_checkpoint $dataset_path/$casename/chkpnt30000.pth --feature_level ${level}
+    python train.py -s $dataset_path -m output/${casename} --start_checkpoint $dataset_path/output/$casename/chkpnt30000.pth --feature_level ${level}
     # e.g. python train.py -s data/sofa -m output/sofa --start_checkpoint data/sofa/sofa/chkpnt30000.pth --feature_level 3
 done
 
